0acf55885667ca01c680c0f2f3faf6bd944b7663
Rodrigo Costa <rodrigocostasantos07@gmail.com>
2024-09-18 07:51:29 -0300

    spam zipkin

--- app1/src/main/java/br/com/seed/otel/app1/controller/MessageController.java
+++ app1/src/main/java/br/com/seed/otel/app1/controller/MessageController.java
@@ -1,16 +1,20 @@
 package br.com.seed.otel.app1.controller;
 
 import br.com.seed.otel.app1.feign.App2Integration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class MessageController {
 
     @Autowired
     private App2Integration app2Integration;
+
 
     @GetMapping("app1/{message}")
     public String getMessage(@PathVariable("message") String message){
